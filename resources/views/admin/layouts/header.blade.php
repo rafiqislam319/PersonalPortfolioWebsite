5ec2b2535b99cd28f7fa247fdd83bf8632ba4ff6
rafiqislam319 <rafiqislam319@gmail.com>
2023-11-08 22:32:54 +0600

    install and customize laravel breeze for authentication

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -26,7 +26,7 @@
 
 <body>
     <div class="container-scroller">
-        
+
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex align-items-top flex-row">
             <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
@@ -35,16 +35,16 @@
                         <span class="icon-menu"></span>
                     </button>
                 </div>
-                
+
             </div>
             <div class="navbar-menu-wrapper d-flex align-items-top">
                 <ul class="navbar-nav">
                     <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
-                        <h1 class="welcome-text">Good Morning, <span class="text-black fw-bold">John Doe</span></h1>
+                        <h1 class="welcome-text text-warning">Good Morning, <span class="text-success fw-bold">{{ Auth::user()->name }}</span></h1>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
-                   
+
                     <li class="nav-item d-none d-lg-block">
                         <div id="datepicker-popup" class="input-group date datepicker navbar-date-picker">
                             <span class="input-group-addon input-group-prepend border-right">
@@ -63,20 +63,33 @@
                         <a class="nav-link count-indicator" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
                             <i class="icon-mail icon-lg"></i>
                         </a>
-                
+
                     </li>
-                    
+
                     <li class="nav-item dropdown d-none d-lg-block user-dropdown">
                         <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img class="img-xs rounded-circle" src="{{ asset('admin') }}/images/faces/face8.jpg" alt="Profile image"> </a>
+                            <img class="img-xs rounded-circle" src="{{ asset('admin') }}/images/faces/profile.jpg" alt="Profile image"> </a>
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
                             <div class="dropdown-header text-center">
-                                <img class="img-md rounded-circle" src="{{ asset('admin') }}/images/faces/face8.jpg" alt="Profile image">
-                                <p class="mb-1 mt-3 font-weight-semibold">Allen Moreno</p>
-                                <p class="fw-light text-muted mb-0">allenmoreno@gmail.com</p>
+                                <img class="img-md rounded-circle" src="{{ asset('admin') }}/images/faces/profile.jpg" alt="Profile image">
+                                <p class="mb-1 mt-3 font-weight-semibold text-warning">{{ Auth::user()->name }}</p>
+                                <p class="fw-light text-muted mb-0">{{ Auth::user()->email }}</p>
                             </div>
-                            <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
-                            <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out</a>
+
+
+                            <a href="{{ route('profile.edit') }}" class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i>
+                                My Profile <span class="badge badge-pill badge-danger">1</span></a>
+
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a class="dropdown-item" :href="route('logout')" onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                    <i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>
+                                    Sign Out
+                                </a>
+
+                            </form>
+
                         </div>
                     </li>
                 </ul>
